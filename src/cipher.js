6f0eab9730f92c38078a876ca1d708ff30841d0c
sara-rioseco <sara.rioseco@gmail.com>
2023-04-13 10:26:24 -0400

    función encode corregida

--- src/cipher.js
+++ src/cipher.js
@@ -6,22 +6,22 @@ const cipher = {
       var char_ascii = char.charCodeAt(char)
       var coded_char = (char_ascii + offset - 65) % 26 + 65 
       if (char_ascii >=32 && char_ascii <=64) {
-        return char
+        coded_msg = coded_msg + char
       } if (char_ascii >=65 && char_ascii <=90) {
-        return String.fromCharCode(coded_char) 
+          coded_msg = coded_msg + String.fromCharCode(coded_char) 
       } if (char_ascii >=97 && char_ascii <=122) {
           var min_char = ((char_ascii - 32) + offset - 65) % 26 + 65
-          return String.fromCharCode(min_char)
+          coded_msg = coded_msg + String.fromCharCode(min_char)
       } if (char === "?") {
-              return "?"
+          coded_msg = coded_msg + "?"
       } if (char === "¿") {
-              return "¿"
+          coded_msg = coded_msg + "¿"
       } if (char === "¡") {
-              return "¡"  
+          coded_msg = coded_msg + "¡"  
       } else {
-                return ""
+          coded_msg = coded_msg + ""
       }
-    }
+    } return coded_msg
   },
 decode: function (txt, offset) {
   var coded_msg = ""
