c01cd402eb2927715d683abec87bd25d4d6dd9d1
sara-rioseco <sara.rioseco@gmail.com>
2023-04-14 15:03:35 -0400

    funcion encode corregida

--- src/cipher.js
+++ src/cipher.js
@@ -1,51 +1,63 @@
 const cipher = {
   encode: function (offset, txt) {
-    let coded_msg = ""
-    for (let i=0; i<txt.length; i++){
-      let char = txt[i]
-      let char_ascii = char.charCodeAt(char)
-      let coded_char = (char_ascii + offset - 65) % 26 + 65 
-      if (char_ascii >=32 && char_ascii <=64) {
-        coded_msg = coded_msg + char
-      } if (char_ascii >=65 && char_ascii <=90) {
-          coded_msg = coded_msg + String.fromCharCode(coded_char) 
-      } if (char_ascii >=97 && char_ascii <=122) {
-          let min_char = ((char_ascii - 32) + offset - 65) % 26 + 65
-          coded_msg = coded_msg + String.fromCharCode(min_char)
-      } if (char === "?") {
-          coded_msg = coded_msg + "?"
-      } if (char === "¿") {
-          coded_msg = coded_msg + "¿"
-      } if (char === "¡") {
-          coded_msg = coded_msg + "¡"  
-      } else {
-          coded_msg = coded_msg + ""
+    let coded_msg = "";
+    for (let i = 0; i < txt.length; i++) {
+      const char = txt[i];
+      const char_ascii = char.charCodeAt(char);
+      const coded_char = ((char_ascii + offset - 65) % 26) + 65;
+      if (char_ascii >= 32 && char_ascii <= 64) {
+        coded_msg = coded_msg + char;
       }
-    } return coded_msg
-  },
-decode: function (offset, txt) {
-  let decoded_msg = ""
-  for (let i=0; i<txt.length; i++){
-    let char = txt[i]
-    let char_ascii = char.charCodeAt(char)
-    let decoded_char = (char_ascii - offset - 65) % 26 + 65 
-    if (char_ascii >=32 && char_ascii <=64) {
-      decoded_msg = decoded_msg + char
-    } if (char_ascii >=65 && char_ascii <=90) {
-        decoded_msg = decoded_msg + String.fromCharCode(decoded_char) 
-    } if (char_ascii >=97 && char_ascii <=122) {
-        let min_char = ((char_ascii - 32) - offset - 65) % 26 + 65
-        decoded_msg = decoded_msg + String.fromCharCode(min_char)
-    } if (char === "?") {
-        decoded_msg = decoded_msg + "?"
-    } if (char === "¿") {
-        decoded_msg = decoded_msg + "¿"
-    } if (char === "¡") {
-        decoded_msg = decoded_msg + "¡"  
-    } else {
-        decoded_msg = decoded_msg + ""
+      if (char_ascii >= 65 && char_ascii <= 90) {
+        coded_msg = coded_msg + String.fromCharCode(coded_char);
+      }
+      if (char_ascii >= 97 && char_ascii <= 122) {
+        const min_char = ((char_ascii - 32 + offset - 65) % 26) + 65;
+        coded_msg = coded_msg + String.fromCharCode(min_char);
+      }
+      if (char === "?") {
+        coded_msg = coded_msg + "?";
+      }
+      if (char === "¿") {
+        coded_msg = coded_msg + "¿";
+      }
+      if (char === "¡") {
+        coded_msg = coded_msg + "¡";
+      } else {
+        coded_msg = coded_msg + "";
+      }
     }
-  } return decoded_msg
-}
-}
+    return coded_msg;
+  },
+  decode: function (offset, txt) {
+    let decoded_msg = "";
+    for (let i = 0; i < txt.length; i++) {
+      const char = txt[i];
+      const char_ascii = char.charCodeAt(char);
+      const decoded_char = ((char_ascii - offset - 65) % 26) + 65;
+      if (char_ascii >= 32 && char_ascii <= 64) {
+        decoded_msg = decoded_msg + char;
+      }
+      if (char_ascii >= 65 && char_ascii <= 90) {
+        decoded_msg = decoded_msg + String.fromCharCode(decoded_char);
+      }
+      if (char_ascii >= 97 && char_ascii <= 122) {
+        const min_char = ((char_ascii - 32 - offset - 65) % 26) + 65;
+        decoded_msg = decoded_msg + String.fromCharCode(min_char);
+      }
+      if (char === "?") {
+        decoded_msg = decoded_msg + "?";
+      }
+      if (char === "¿") {
+        decoded_msg = decoded_msg + "¿";
+      }
+      if (char === "¡") {
+        decoded_msg = decoded_msg + "¡";
+      } else {
+        decoded_msg = decoded_msg + "";
+      }
+    }
+    return decoded_msg;
+  },
+};
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import cipher from './cipher.js';
-    const cdform = document.getElementById("codeform");
-    const button = document.getElementById("bttn");
-        button.addEventListener('click'), (event) => {
-            const msg = document.getElementById("inputtext").value;
-            const ofst = document.getElementById("inputoffset").value;
-            const encoded = cipher.encode(ofst, msg);
-            const decoded = cipher.decode(ofst, msg);
-            event.preventDefault();
-            console.log(encoded);
-            console.log(decoded);
-        }
+import cipher from "./cipher.js";
+const button = document.getElementById("bttn");
+button.addEventListener("click", (event) => {
+  const msg = document.getElementById("inputtext").value;
+  const ofst = document.getElementById("inputoffset").value;
+  console.log(msg);
+  console.log(ofst);
+  const encoded = cipher.encode(Number(ofst), msg);
+  const decoded = cipher.decode(Number(ofst), msg);
+  event.preventDefault();
+  console.log(encoded);
+  console.log(decoded);
+});
