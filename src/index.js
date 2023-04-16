72936725de50ff191448f50c8d9a2b98855e81d9
sara-rioseco <sara.rioseco@gmail.com>
2023-04-16 16:55:26 -0400

    boton corregido

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ const button = document.getElementById("bttn");
 button.addEventListener("click", (event) => {
   const msg = document.getElementById("inputtext").value;
   const ofst = document.getElementById("inputoffset").value;
-  console.log(msg);
-  console.log(ofst);
   const encoded = cipher.encode(Number(ofst), msg);
   const decoded = cipher.decode(Number(ofst), msg);
   event.preventDefault();
