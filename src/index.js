7ec79e1715a182eeb5887b73ee3332955ead27de
sara-rioseco <sara.rioseco@gmail.com>
2023-04-16 18:45:29 -0400

    funcionalidad decode

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ button.addEventListener("click", (event) => {
   const msg = document.getElementById("inputtext").value;
   const ofst = document.getElementById("inputoffset").value;
   const encoded = cipher.encode(Number(ofst), msg);
-  const decoded = cipher.decode(Number(ofst), msg);
   event.preventDefault();
   console.log(encoded);
+});
+const button2 = document.getElementById("bttn2");
+button2.addEventListener("click", (event) => {
+  const msg2 = document.getElementById("codedtext").value;
+  const ofst2 = document.getElementById("codedoffset").value;
+  const decoded = cipher.decode(Number(ofst2), msg2);
+  event.preventDefault();
   console.log(decoded);
 });
